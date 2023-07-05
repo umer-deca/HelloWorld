de08242b4d5584a365fcccbf5a483e4bc54a3508
Umer <umer.farooq@decagames.com>
2023-07-05 21:23:36 +0500

    added java project

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Hello World...");
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello World..." + i);
+		}
+	}
+}
